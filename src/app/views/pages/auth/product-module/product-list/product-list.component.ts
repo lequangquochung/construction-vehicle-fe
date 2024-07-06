58f61e895febbd3cdd3f4e1cdde72089e452a7a3
hunglqq <lequangquochung1912@gmail.com>
2024-07-06 19:56:45 +0700

    update 6/7

--- src/app/views/pages/auth/product-module/product-list/product-list.component.ts
+++ src/app/views/pages/auth/product-module/product-list/product-list.component.ts
@@ -1,8 +1,11 @@
+import { map } from 'rxjs';
 import { ProductService } from './../../../../../services/product/product.service';
 import { NgFor } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { SpinnerModule } from '@coreui/angular';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ElementRefDirective, SpinnerModule, TooltipDirective } from '@coreui/angular';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { environment } from 'src/environments/environment';
+import { faPencil, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-product-list',
@@ -10,21 +13,36 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   styleUrl: 'product-list.component.scss',
   standalone: true,
   imports: [NgFor, FontAwesomeModule,
-    SpinnerModule,]
+    SpinnerModule,
+    TooltipDirective,
+    ElementRefDirective]
 })
 export class ProductListComponent implements OnInit{
   keyword: string = "";
   products: any = [];
+  baseApi = environment.APIURL;
+
+  @ViewChild('inputFile') inputFile!: ElementRef;
+  faEdit = faPencil;
+  faDelete = faTrashCan;
+  
   constructor(private productService: ProductService) {}
   ngOnInit(): void {
-    
+    this.getAll();
   }
 
   getAll() {
     this.productService.getAll(this.keyword).subscribe({
       next: (res) => {
-        this.products = res.data.data;
+        console.log(res);
+        this.products = res.data.data.map((item: any) => {
+          item.image = `${this.baseApi}/${item.image}`
+          return item;
+        });
       }
     })
   }
+
+  toggleEditModal(id?: string) {}
+  toggleDelete(id?: string){}
 }
