0b5f5d0ea63ac25a36d4c601436277ebe02cf664
hunglqq <lequangquochung1912@gmail.com>
2024-07-15 16:59:18 +0700

    update carousel

--- src/app/views/pages/client-page/nav-header/nav-header.component.ts
+++ src/app/views/pages/client-page/nav-header/nav-header.component.ts
@@ -1,6 +1,8 @@
+import { NgFor } from '@angular/common';
 import { AddToCartService } from './../../../../services/client-service/add-to-cart/add-to-card.service';
 import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CarouselComponent, CarouselControlComponent, CarouselInnerComponent, CarouselItemComponent } from '@coreui/angular';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCartPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
 
@@ -10,12 +12,25 @@ import { faCartPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
   styleUrl: 'nav-header.component.scss',
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [RouterModule, FontAwesomeModule]
+  imports: [RouterModule, FontAwesomeModule,
+    CarouselComponent,
+    CarouselControlComponent,
+    CarouselInnerComponent,
+    CarouselItemComponent,
+    NgFor
+  ]
 })
 export class NavHeaderComponent implements OnInit {
   pageLink = {
     aboutUs: "about-us"
   };
+
+  slides: Slides[] = [
+    {
+      title: "",
+      src: "/assest/"
+    }
+  ];
 
   faIcon = {
     faCartPlus: faCartPlus,
@@ -38,7 +53,7 @@ export class NavHeaderComponent implements OnInit {
       this.getCountItem();
     });
     this.addToCartService.isChangedData$.subscribe(data => {
-      if(data) {
+      if (data) {
         this.getCountItem();
       };
     })
@@ -54,4 +69,9 @@ export class NavHeaderComponent implements OnInit {
     let data: Array<string> = value ? JSON.parse(value) : null;
     this.cartCount = data?.length ? data?.length : 0;
   }
+}
+
+export class Slides {
+  title: string| undefined;
+  src: string | undefined;
 }
